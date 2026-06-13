55594aa08d3651b41f949673a03cfccb154778fa
Stefani Signore <stefanisignore@gmail.com>
2026-06-13 20:16:41 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/itau/transferAPI/TransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.transferAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferApiApplication.class, args);
+	}
+
+}
