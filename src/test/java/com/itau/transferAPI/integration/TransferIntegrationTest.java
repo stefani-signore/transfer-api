c9d42623872619d511ff43b3ac4dbab27c445d03
stefani-signore <stefanisignore@gmail.com>
2026-06-15 02:38:54 -0300

    refactor: ajusta application.properties e arquivo de teste de integracao

--- src/test/java/com/itau/transferAPI/integration/TransferIntegrationTest.java
+++ src/test/java/com/itau/transferAPI/integration/TransferIntegrationTest.java
@@ -1,12 +1,59 @@
 package com.itau.transferAPI.integration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 class TransferIntegrationTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void shouldTransferSuccessfully() throws Exception {
+
+        mockMvc.perform(post("/api/v1/clients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                      "name":"Origem",
+                      "accountNumber":"11111",
+                      "balance":5000
+                    }
+                    """))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/api/v1/clients")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                      "name":"Destino",
+                      "accountNumber":"22222",
+                      "balance":1000
+                    }
+                    """))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/api/v1/transfers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                    {
+                      "sourceAccount":"11111",
+                      "destinationAccount":"22222",
+                      "amount":1000
+                    }
+                    """))
+                .andExpect(status().isCreated());
+
     }
 }
