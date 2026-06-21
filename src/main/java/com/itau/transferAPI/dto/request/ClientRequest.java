3d4b9deff9a1968573e16a079447f5eb33d01f9e
stefani-signore <stefanisignore@gmail.com>
2026-06-20 23:47:33 -0300

    docs: adiciona descrições e exemplos aos requests no Swagger

--- src/main/java/com/itau/transferAPI/dto/request/ClientRequest.java
+++ src/main/java/com/itau/transferAPI/dto/request/ClientRequest.java
@@ -1,5 +1,6 @@
 package com.itau.transferAPI.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -9,13 +10,22 @@ import java.math.BigDecimal;
 public record ClientRequest(
 
         @NotBlank
+        @Schema(
+                example = "Stefani",
+                description = "Nome do cliente")
         String name,
 
         @NotBlank
+        @Schema(
+                example = "11111",
+                description = "Número único da conta")
         String accountNumber,
 
         @NotNull
         @PositiveOrZero
+        @Schema(
+                example = "5000.00",
+                description = "Saldo inicial da conta")
         BigDecimal balance
 
 ) {
--- src/main/java/com/itau/transferAPI/dto/request/TransferRequest.java
+++ src/main/java/com/itau/transferAPI/dto/request/TransferRequest.java
@@ -1,5 +1,6 @@
 package com.itau.transferAPI.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,13 +10,22 @@ import java.math.BigDecimal;
 public record TransferRequest(
 
         @NotBlank
+        @Schema(
+                example = "11111",
+                description = "Número de origem da conta")
         String sourceAccount,
 
         @NotBlank
+        @Schema(
+                example = "22222",
+                description = "Número da conta destino")
         String destinationAccount,
 
         @NotNull
         @DecimalMin("0.01")
+        @Schema(
+                example = "1000.00",
+                description = "Quantia a ser transferida")
         BigDecimal amount
 
 ) {
